2db9e2837eada72361b37a9f913ab2b74e7750b3
Orlando <isc.orangelc@gmail.com>
2020-02-11 08:46:53 -0600

    add beer_song.py file and new version of odds.py

--- odds.py
+++ odds.py
@@ -4,7 +4,8 @@ import random
 import time
 from datetime import datetime
 
-odds = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59]
+odds = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,
+        31,33,35,37,39,41,43,45,47,49,51,53,55,57,59]
 
 for i in range(5):
     right_this_minute = datetime.today().minute
@@ -15,4 +16,4 @@ for i in range(5):
         print("Not an odd minute.")
     wait_time = random.randint(1,60)
     print("Tiempo de espera: " + str(wait_time))
-    time.sleep(wait_time)
+    time.sleep(1)
